0d50d6f1fa8e3094ae755594ef2a3d050aaac8b5
linda-20 <lindaaron40@gmail.com>
2024-02-02 12:27:47 +0300

    Completed asssignment

---
+++ src/components/ArticleList.js
@@ -0,0 +1,15 @@
+// ArticleList.js
+import React from 'react';
+import Article from './Article';
+
+const ArticleList = ({ posts }) => {
+  return (
+    <main>
+      {posts.map((post, index) => (
+        <Article key={index} {...post} />
+      ))}
+    </main>
+  );
+};
+
+export default ArticleList;
